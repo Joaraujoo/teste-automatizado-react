46335727e8d1e626d0c0d63cebcfe44b252b825c
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-06-28 12:45:42 -0300

    coments

--- src/User/User.test.tsx
+++ src/User/User.test.tsx
@@ -10,7 +10,7 @@ describe("User component", () => {
     it("should check if the name is not displayerd when component is mounted", () => {
         render(<User/>)
 
-        const message = screen.queryByText("Usuário online: Joao Araujo")
+        const message = screen.queryByText("Usuário online: Joao Araujo") //Busca o elemento na tela que possa ou não estar presente no DOM
 
         expect(message).not.toBeInTheDocument()
     })
@@ -18,10 +18,10 @@ describe("User component", () => {
     it("should display the name after typing in the input and click on the button", () => {
         render(<User/>)
 
-        const inputElement = screen.getByPlaceholderText("Digite o nome")
+        const inputElement = screen.getByPlaceholderText("Digite o nome") //Busca o elemente utilizando o placeholder 
         const buttonElement = screen.getByRole("button", { name: "Cadastrar"})
 
-        fireEvent.change(inputElement, {target: {value: "Joao Araujo"}})
+        fireEvent.change(inputElement, {target: {value: "Joao Araujo"}})//Simula a mudança de valor do input
         fireEvent.click(buttonElement)
 
         const message = screen.getByText("Usuário online: Joao Araujo")
